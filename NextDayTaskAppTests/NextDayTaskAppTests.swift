555e86c389d877e11b59d1f2d6732382aec8b237
Sourabh <modisourabh31@gmail.com>
2025-03-31 18:37:52 +0530

    Initial Commit

---
+++ NextDayTaskAppTests/NextDayTaskAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NextDayTaskAppTests.swift
+//  NextDayTaskAppTests
+//
+//  Created by Sourabh Modi on 31/03/25.
+//
+
+import Testing
+@testable import NextDayTaskApp
+
+struct NextDayTaskAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
